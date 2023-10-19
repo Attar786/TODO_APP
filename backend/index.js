bc3f0bd1809b7fe6962cb2eb28321e417afecea6
Atta ur Rehman <attaurr786@outlook.com>
2023-10-19 21:19:03 +0500

    data bases connected to data bases

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const app = express();
 
 const PORT = process.env.port || 5000;
 
-
+mongoose.connect(process.env.MONGOURL)
+.then(()=>
+{
+    console.log(`Connected to data bases of mongo db`)
+}).catch((err)=> console.log(err))
 app.listen(PORT, ()=> console.log(`app lining on = ${PORT}`))
 
 
